407600068c268c1a70a09f29fb950e057f068381
Rodymus <robertogonzalez.ar@gmail.com>
2024-09-26 19:32:21 -0300

    Comenzando con el proyecto de OOP Oracle One.

---
+++ src/Pelicula.java
@@ -0,0 +1,28 @@
+public class Pelicula {
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
+
+    int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
+
+    void muestraFichaTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: " + duracionEnMinutos);
+
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones/totalDeLasEvaluaciones;
+    }
+}
